2396f164a81ecca17501a23096dd8a2f47726558
herohit <rohitnegi323@gmail.com>
2024-02-20 17:13:57 +0530

    added send_otp and generate otp

--- EmailAuth/authentication/utils.py
+++ EmailAuth/authentication/utils.py
@@ -1,6 +1,25 @@
 import random
 import string
 
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+
 def generate_otp():
     otp = ''.join(random.choices(string.digits, k=6))
     return otp
+
+
+def send_otp_email(email, otp):
+    subject = 'Verification Code'
+    message = f'Your verification code is: {otp}'
+    sender_email = settings.EMAIL_HOST_USER
+
+    try:
+        send_mail(subject, message, sender_email, [email])
+        return True
+    except Exception as e:
+        # Log the error or handle it appropriately
+        print(f"Error sending email: {str(e)}")
+        return False
