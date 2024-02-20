6fe89bd0ffef04b9c25334994fdf6234c1a152ce
herohit <rohitnegi323@gmail.com>
2024-02-20 17:12:47 +0530

    made verify otp view and changed register logic

--- EmailAuth/authentication/views.py
+++ EmailAuth/authentication/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from .forms import CustomUserCreationForm 
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout as auth_logout,login as auth_login,authenticate
@@ -7,12 +8,17 @@ from .models import Profile
 
 from django.contrib.auth.models import User
 from .utils import generate_otp
+from .utils import send_otp_email
+from django.contrib import messages
+
+from .decorator import email_verified_required
 
 #homepage
 @login_required(login_url='/login/')
 def home(request):
+    current_user_profile = Profile.objects.get(user=request.user)
     username = request.user.username
-    context={'username':username}
+    context={'username':username,'current_user_profile':current_user_profile}
     return render(request,'homepage.html',context) 
 
 
@@ -43,7 +49,13 @@ def register(request):
                 user = authenticate(request, username=username, password=password)
                 if user is not None:
                     # Log in the user
-                    login(request, user)
+                    auth_login(request, user)
+
+                    # if not send_otp_email(email, otp):
+                    # Failed to send email, handle error
+                        # return HttpResponse('Failed to send OTP email. Please try again later.')
+
+
 
                 # Redirect to the OTP verification page
                 return redirect('verify_otp')
@@ -53,7 +65,35 @@ def register(request):
 
 
 def verify_otp(request):
-    return render(request,'verify_otp.html')
+    if request.method == 'POST':
+        entered_otp = request.POST.get('otp')
+
+        
+        profile = Profile.objects.get(user=request.user)
+
+        # Verify the entered OTP
+        if entered_otp == profile.email_verification_code:
+            profile.is_email_verified = True
+            profile.save()
+
+
+            messages.success(request, 'Email verification successful! Now you have full access to site')
+            return redirect('home')
+        else:
+            # OTP is incorrect
+            messages.error(request, 'Invalid OTP. Please try again.')
+            return render(request, 'verify_otp.html')
+
+    
+    return render(request, 'verify_otp.html')
+
+
+
+@login_required
+@email_verified_required
+def edit_profile(request):
+    return render(request,'edit_profile.html')
+
 
 # login view
 def login(request):
